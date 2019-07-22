008e0868d9ecaeec56729aadbabb4bb98fe4ddb0
simon <simon@movi.ai>
2019-07-22 22:02:01 +0200

    Added hex, bin, oct literals, cleaned up error

--- README.md
+++ README.md
@@ -6,11 +6,15 @@ Like my previous calc project, only written in Rust. I use it as a tool for my o
 This is cleaner, maybe faster, and offers new functionality.
 
 ### Usage
-Right now, single expressions are possible in a one-shot command, e.g. `rcalc "5 / 3"`.
+Right now, single expressions are possible in a one-shot command:
+```
+rcalc "5 / 3"
+rcalc "0b4 * 0x2d"
+```
 
 Also, the interpreter can be entered by simply running `rcalc`, then multiple expressions can be evaluated.
 
-In interpreted mode, variables can be used to save values.
+In interpreted mode, variables can be used to save values:
 ```
 v = 5 / 3
 v * 20
--- src/error.rs
+++ src/error.rs
@@ -14,8 +14,8 @@ impl Display for RuntimeError {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         use self::RuntimeError::*;
         match self {
-            AnsNotDefined       => write!(f, "{}", "ans is not defined. Run at least one expression before using this variable."),
-            VarNotDefined(v)    => write!(f, "{}", format!("variable {var} is not defined. Set it with {var} = <expression>.", var = v).as_str())
+            AnsNotDefined       => write!(f, "ans is not defined. Run at least one expression before using this variable."),
+            VarNotDefined(v)    => write!(f, "variable {var} is not defined. Set it with {var} = <expression>.", var = v)
         }
     }
 }
